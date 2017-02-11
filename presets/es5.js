35fbd5acf39bfe86483cc6df801ee5c103c50041
Ben Teichman <ben.l.teichman@gmail.com>
2017-02-11 12:38:10 -0500

    feat(presets): presets available through module reference

---
+++ presets/es5.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line immutable/no-mutation
+module.exports = require('../build/presets/es5').default();
---
+++ presets/es6.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line immutable/no-mutation
+module.exports = require('../build/presets/es6').default();
